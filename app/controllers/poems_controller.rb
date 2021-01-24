120b8e12f33ad8229a59baafdb0cc2101d6d5a88
github username <github email address>
2021-01-24 10:30:43 -0800

    added new comment and index actions to poems controller

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,2 +1,14 @@
 class PoemsController < ApplicationController
+
+	def new_comment_form
+		render partial: 'poems/new_comment_form'
+	end
+
+	def index
+		@poems = Poem.all
+		respond_to do |f|
+			f.html {render :index}
+			f.json {render json: @poems}
+		end
+	end
 end
