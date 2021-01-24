a307e61ec28abefaba640a13965f84881dc3a5ce
github username <github email address>
2021-01-24 12:17:35 -0800

    added index to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def index
+		@users = User.all
+		respond_to do |f|
+			f.html {render :index} 
+			f.json {render json: @users}
+		end
+    end
+    
+    
 end
