e3e11c55d365668a01b0c98c94f71c4197c443fb
github username <github email address>
2021-01-24 12:20:09 -0800

    added new action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 			f.json {render json: @users}
 		end
     end
-    
+
     def show
 		@user = User.find(params["id"])
 		respond_to do |f|
@@ -15,5 +15,8 @@ class UsersController < ApplicationController
 		end
 	end
 	
-
+    def new
+		@poem = Poem.find(params["poem_id"])
+		@user = @poem.users.build
+	end
 end
