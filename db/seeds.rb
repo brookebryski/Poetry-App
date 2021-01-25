170c7a1f813dfb02c6847b9a764eb8e989a72d4a
github username <github email address>
2021-01-25 09:14:03 -0800

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,7 @@
-# User.create(username: 'brooke', email: 'brooke@email.com')
-# User.create(username: 'bethany', email: 'bethany@email.com')
-# User.create(username: 'ashley', email: 'ashley@email.com')
-
-# Poem.create(user_id: 1, title: "Baby", body: "Baby, baby, baby / Oh / Baby, baby, baby / No / Baby, baby, baby / Oh / Thought You'd always be mine")
-# Poem.create(user_id: 1, title: "On The Sea", body: "On the Sea / All is Forgiven")
-# Poem.create(user_id: 1, title: "Space Song", body: "It was late at night / You held on tight / To a narraow / Ray of light")
-
-# Poem.create(user_id: 2, title: "LA", body: "City of Angels / What have you up your sleeve? / City of Angeles / What am I supposed to believe?")
-# Poem.create(user_id: 2, title: "Astronaut", body: "Fall / Back / In / To / Place")
-# Poem.create(user_id: 2, title: "Lemon", body: "Even with the lights down low / Lemon colored heartbeat glow")
-
-# Poem.create(user_id: 3, title: "Saturn", body: "I was coming closer / To a new world order")
-# Poem.create(user_id: 3, title: "Party", body: "Tiny little ray of light / Where will you lead me to tonight?")
-# Poem.create(user_id: 3, title: "Good Time", body: "I had a good time / Making something out of nothing / If only for a short time")
-
-
-# Comment.create(user_id: 1, poem_id: 1, content: 'I loved this poem!')
-# Comment.create(user_id: 2, poem_id: 1, content: 'Excellent work!')
-# Comment.create(user_id: 3, poem_id: 1, content: 'Such a beautiful poem!')
-
-# Comment.create(user_id: 1, poem_id: 2, content: 'Stunning!')
-# Comment.create(user_id: 2, poem_id: 2, content: 'Wonderful piece!')
-# Comment.create(user_id: 3, poem_id: 2, content: 'Phenomenal poem.')
-
-# Comment.create(user_id: 1, poem_id: 3, content: 'Beautiful work.')
-# Comment.create(user_id: 2, poem_id: 3, content: 'I adored this poem!')
-# Comment.create(user_id: 3, poem_id: 3, content: 'Incredible!')
-
-# Comment.create(user_id: 1, poem_id: 1, content: 'Wonderful!')
-# Comment.create(user_id: 2, poem_id: 1, content: 'Amazing.')
-# Comment.create(user_id: 3, poem_id: 1, content: 'Beautiful piece of work.')
-
-# Comment.create(user_id: 1, poem_id: 2, content: 'I loved this!')
-# Comment.create(user_id: 2, poem_id: 2, content: 'Keep up the good work!')
-# Comment.create(user_id: 3, poem_id: 2, content: 'Such a gorgeous piece')
-
-# Comment.create(user_id: 1, poem_id: 3, content: 'Love this poem!')
-# Comment.create(user_id: 2, poem_id: 3, content: 'Amazing!')
-# Comment.create(user_id: 3, poem_id: 3, content: 'Great job!')
-
-# Comment.create(user_id: 1, poem_id: 1, content: 'Such good work!')
-# Comment.create(user_id: 2, poem_id: 1, content: 'My favorite work of yours!')
-# Comment.create(user_id: 3, poem_id: 1, content: 'Wonderful!')
-
-# Comment.create(user_id: 1, poem_id: 2, content: 'You are such a great poet!')
-# Comment.create(user_id: 2, poem_id: 2, content: 'Well done!')
-# Comment.create(user_id: 3, poem_id: 2, content: 'I enjoyed this so much!')
-
-# Comment.create(user_id: 1, poem_id: 3, content: 'Phenomenal!')
-# Comment.create(user_id: 2, poem_id: 3, content: 'Great job on this!')
-# Comment.create(user_id: 3, poem_id: 3, content: 'Cannot wait to see more of your work!')
-
+poems = 
+[
+    {title: "The Red Wheelbarrow", author: "William Carlos Williams", link: "https://www.poetryfoundation.org/poems/45502/the-red-wheelbarrow" image: "https://media.poetryfoundation.org/uploads/media/default/0001/22/b1538427e0ee77471a396be295eb85b4baa27e5a.jpeg?w=690&h=&fit=max&690"}
+    {title: "The Road Not Taken", author: "Robert Frost", link: "https://www.poetryfoundation.org/poems/44272/the-road-not-taken" image: "https://media.poetryfoundation.org/uploads/media/default/0001/22/e4e2b4ab2045540b0b12862a3dd6276c086476c4.jpeg?w=690&h=&fit=max&690"}
+    {title: "Because I could not stop for Death-", author: "Emily Dickinson", link: "https://www.poetryfoundation.org/poems/47652/because-i-could-not-stop-for-death-479" image: "https://media.poetryfoundation.org/uploads/media/default/0001/22/c1140d659d1ae1fc5db36c989bcf37941dfca9ee.jpeg?w=260&h=&fit=max&260"}
+    {title: "Harlem", author: "Langston Hughes, link: "https://www.poetryfoundation.org/poems/46548/harlem" image: "https://media.poetryfoundation.org/uploads/media/default/0001/21/243b5a66eaf2658e5d804be75f1d2a9436bebfe0.jpeg?w=690&h=&fit=max&690"}
+]
