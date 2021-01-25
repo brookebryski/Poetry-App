ad432fb9d199e79682ed540bfd8a5a803c8fba44
github username <github email address>
2021-01-25 09:23:29 -0800

    edited poems controller

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,40 +1,15 @@
 class PoemsController < ApplicationController
-	
 	def index
-		@poems = Poem.all
-	
-		render json: @poems, status: 200
-	  end
-	  
-	  def show
-		@poem = Poem.find(params[:id])
-		
-		render json: @poem, status: 200
-	  end
-	  
-	  def create
-		@poem = Poem.create(poem_params)
-		
-		render json: @poem, status: 200
-	  end
-	  
-	  def update
-		@poem = Poem.find(params[:id])
-		@poem.update(poem_params)
-		render json: @poem, status: 200
-	  end
-	  
-	  def destroy 
-		@poem = Poem.find(params[:id])
-		@poem.delete
-	
-		render json: {poemId: @poem.id}
-	  end
-	
-	
-	  private
-		def poem_params
-		  params.require(:poem).permit(:body)
-		end
-	end
+        poems = Poem.all
+        render json: poems, except: [:created_at, :updated_at]
+    end
 
+    def show
+        poem = Poem.find_by(id: params[:id])
+        if poem
+            render json: poems, except: [:created_at, :updated_at]
+        else
+            render json: {message: "Poem not found."}
+        end
+    end
+end
