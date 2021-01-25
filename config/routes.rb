01703c7b7b2f384f710dd483c974f43bf2ac11ec
github username <github email address>
2021-01-25 10:13:27 -0800

    edited config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :poems do
-    resources :comments
-  end
-  
+  resources :poems
+  resources :favorites
+  resources :users do
+    resources :favorites
+  end 
 end
