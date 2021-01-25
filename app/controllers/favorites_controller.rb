4878c2b86c87e50221892e0e315c372414333e8f
github username <github email address>
2021-01-25 09:21:27 -0800

    edited favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,33 @@
 class FavoritesController < ApplicationController
+    def new
+        favorite = Favorite.new
+    end
+
+    def create
+        favorite = Favorite.new(favorite_params)
+        if favorite.save
+            render json: favorite, except: [:created_at, :updated_at]
+        else
+            render json: {message: "Favorite Failed"}
+        end
+    end
+
+    def index
+        user_id = params[:user_id]
+        user = User.find(user_id)
+        favorites = user.favorites
+        render json: favorites, include: [:poem]  
+    end
+
+    def destroy
+        fav_id = params[:id]
+        favorite = Favorite.find(fav_id)
+        favorite.destroy
+    end
+
+
+private
+    def favorite_params
+      params.require(:favorite).permit(:user_id, :poem_id)
+    end
 end
